7ddb4631a3690ce068517022599917302c985185
Mary1-com <vnnkvmrn76@gmail.com>
2026-06-06 01:32:02 +0300

    Disable page scroll when modal is open

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,31 +1,34 @@
-
 "use client";
 
-import { useEffect, ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { createPortal } from "react-dom";
 import css from "./Modal.module.css";
 
-interface ModalProps {
-    children: ReactNode;
-    onClose: () => void;
-    }
+interface NoteModalProps { children: ReactNode; onClose: () => void; }
 
-export default function Modal({ children, onClose, }: ModalProps) {
+export default function NoteModal({ children, onClose }: NoteModalProps) {
+    useEffect(() => {
+        const originalBodyOverflow = document.body.style.overflow;
+        
+        document.body.style.overflow = "hidden";
+
+        return () => {
+            document.body.style.overflow = originalBodyOverflow;
+        };
+    }, []);
+    
     useEffect(() => {
         const handleEsc = (event: KeyboardEvent) => {
-            if (event.key === "Escape") {
-                onClose();
-            }
+            if (event.key === "Escape") { onClose(); }
         };
-
+        
         window.addEventListener("keydown", handleEsc);
 
         return () => {
             window.removeEventListener("keydown", handleEsc);
         };
     }, [onClose]);
-        
-
+    
     return createPortal(
         <div className={css.backdrop} onClick={onClose}>
             <div className={css.modal} onClick={(event) => event.stopPropagation()}>
