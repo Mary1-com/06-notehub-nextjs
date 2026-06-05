81f6884a24947b54aff8ecfb19dcb025b229c3b3
Mary1-com <vnnkvmrn76@gmail.com>
2026-06-05 19:47:09 +0300

    Commit after second  checking after add token to vercel

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,44 +1,9 @@
-// "use client";
-
-// import css from "./NoteModal.module.css";
-// import { useEffect } from "react";
-// import { createPortal } from "react-dom";
-
-// interface ModalProps {
-//   children: React.ReactNode;
-//   onClose: () => void;
-// }
-
-// const modalRoot = document.getElementById("modal-root")!;
-// export default function Modal({ children, onClose }: ModalProps) {
-//     useEffect(() => {
-//         const handleEsc = (event: KeyboardEvent) => {
-//             if (event.key === "Escape") { onClose() }
-//         };
-//     window.addEventListener("keydown", handleEsc);
-//     document.body.style.overflow = "hidden";
-//     return () => {
-//         window.removeEventListener("keydown", handleEsc);
-//             document.body.style.overflow = "auto";
-//         };
-//     }, [onClose]);
-
-//     const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
-//         if (event.target === event.currentTarget) { onClose(); }
-//     };
-    
-//     return createPortal(
-//         <div className={css.backdrop} role="dialog" aria-modal="true" onClick={handleBackdropClick}>
-//             <div className={css.modal}> {children} </div>
-//         </div>,
-//         modalRoot);
-// }
 
 "use client";
 
 import { useEffect, ReactNode } from "react";
 import { createPortal } from "react-dom";
-import css from "./NoteModal.module.css";
+import css from "./Modal.module.css";
 
 interface ModalProps {
     children: ReactNode;
