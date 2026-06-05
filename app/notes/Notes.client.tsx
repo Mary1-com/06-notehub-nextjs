f795110bad1fbad35fdbdd82d61b4fd48daf7ad5
Mary1-com <vnnkvmrn76@gmail.com>
2026-06-05 18:04:37 +0300

    Notes list, search and pagination working - ok

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -5,30 +5,39 @@ import { fetchNotes } from "@/lib/api";
 import NoteList from "@/components/NoteList/NoteList";
 import { useState } from "react";
 import SearchBox from "@/components/SearchBox/SearchBox";
+import Pagination from "@/components/Pagination/Pagination";
 
 export default function NotesClient() {
     const [page, setPage] = useState(1);
     const [search, setSearch] = useState("");    
-    
-    // const { data, isLoading, error } = useQuery({
-    //     queryKey: ["notes", page, search],
-    //     queryFn: () => fetchNotes({ page, search }),
-    // });
     const { data, isLoading, error } = useQuery({
         queryKey: ["notes", page, search],
         queryFn: () => fetchNotes({ page, search }),
         placeholderData: (previousData) => previousData,
     });
-        
-    // if (isLoading) { return <p>Loading...</p>; }
-    // if (error) { return <p>Something went wrong.</p>; }
+    const notes = data?.notes ?? [];
+    const totalPages = data?.totalPages ?? 0;
+    const handleSearch = (value: string) => {
+        setSearch(value);
+        setPage(1);
+    };
+
+
     return (
         <>
             <SearchBox onSearch={setSearch} /> 
+            {totalPages > 1 && (
+                <Pagination
+                pageCount={totalPages}
+                currentPage={page}
+                onPageChange={setPage}
+                />
+            )}
             {isLoading && <p>Loading, please wait...</p>}
             {error && <p>Something went wrong.</p>}
             <h1>Notes</h1>
             <NoteList notes={data?.notes ?? []} />
+            {/* <NoteList notes={notes} /> */}
 
         {/* Pagination */}
         {/* NoteList */}
