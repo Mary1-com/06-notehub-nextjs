820ce2b7f57e2f0e07b8f2a67b3e7059e90292c5
Mary1-com <vnnkvmrn76@gmail.com>
2026-06-05 16:49:34 +0300

    Notes page displays notes list - ok

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -2,24 +2,25 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api";
+// import SearchBox from "@/components/SearchBox/SearchBox";
+// import Pagination from "@/components/Pagination/Pagination";
+import NoteList from "@/components/NoteList/NoteList";
 import { useState } from "react";
 import SearchBox from "@/components/SearchBox/SearchBox";
-import Pagination from "@/components/Pagination/Pagination";
-import NoteList from "@/components/NoteList/NoteList";
-import css from "./NotesPage.module.css";
+// import css from "./NotesPage.module.css";
 
 export default function NotesClient() {
-    const [page, setPage] = useState(1);
+    // const [page, setPage] = useState(1);
     const [search, setSearch] = useState("");    
     
     const { data, isLoading, error } = useQuery({
-        queryKey: ["notes"],
-        queryFn: () =>
-            fetchNotes({
+        queryKey: ["notes", search],
+        queryFn: () => fetchNotes({
             page: 1,
-            search: "",
-            }),
+            search,
+        }),
     });
+    
 
 
 
@@ -28,9 +29,10 @@ export default function NotesClient() {
     if (error) { return <p>Something went wrong.</p>; }
     return (
         <>
-        <h1>Notes</h1>
-        <p>Found notes: {data?.notes.length}</p>
-        {/* SearchBox */}
+            <SearchBox onSearch={setSearch} />   
+            <h1>Notes</h1>
+            <NoteList notes={data?.notes ?? []} />
+
         {/* Pagination */}
         {/* NoteList */}
         </>
